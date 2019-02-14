07502428132c97f73dd18b6b49daba3d6f67e7a0
Ryan <ryan4652@hawaii.edu>
2019-02-14 10:28:34 -1000

    Completed Arrays

--- SwiftPlaygroundThree.playground/Contents.swift
+++ SwiftPlaygroundThree.playground/Contents.swift
@@ -1,3 +1,22 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//Part 2
+var fruitNames: [String] = ["Pineapple", "Mango", "Watermelon"]
+
+print(fruitNames)
+
+// This is String Interpolation
+print("I like to eat \(fruitNames)")
+
+//This is Arrays
+for name in fruitNames{
+print("I like to eat " + name)
+}
+
+//Printing Individual Fruits
+print(fruitNames[0])
+print(fruitNames[1])
+print(fruitNames[2])
+
