d87e01b596d7d84eec4992f2bd5f02794480e711
Ryan <ryan4652@hawaii.edu>
2019-02-14 10:51:09 -1000

    Completed - Favorited Numbers

--- SwiftPlaygroundThree.playground/Contents.swift
+++ SwiftPlaygroundThree.playground/Contents.swift
@@ -20,3 +20,18 @@ print(fruitNames[0])
 print(fruitNames[1])
 print(fruitNames[2])
 
+//Assignment #4 Starts Here
+
+//Part 8, Start of Assignments #4
+
+var numbers: [Int] = [1, 5, 7, 8, 9]
+print(numbers)
+
+print("I like \(numbers)")
+
+for myFavoriteNumbers in numbers {
+    print("My favorite number", (myFavoriteNumbers))
+    
+}
+
+
